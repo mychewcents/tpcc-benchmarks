f90efc5deaba02a08ae8ab95bdd9e1293d4bed90
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-10-09 14:45:36 +0530

    Renaming server script file

--- cockroachdb/scripts/server.sh
+++ cockroachdb/scripts/server.sh
@@ -1,48 +1,74 @@
 #! /bin/sh
 
 h=$(hostname -s | cut -c5-6)
+action=$1
 
-if [ $h == 30 ]
+if [ $1 == "start" ]
+then
+  echo "Starting Cockroach DB node on : node${h}"
+  if [ $h == 30 ]
   then
-    cockroach start --store=node0 \
+    cockroach start --insecure --store=node0 \
       --listen-addr=localhost:30000 \
-      --http-addr=localhost:40000
+      --http-addr=localhost:40000 \
       --join=xcnc31.comp.nus.edu.sg:30001,xcnc32.comp.nus.edu.sg:30002,xcnc33.comp.nus.edu.sg:30003,xcnc34.comp.nus.edu.sg:30004 \
       --background
 
+    cockroach init --insecure --host=localhost:30000
+
   elif [ $h == 31 ]
   then
-    cockroach start --store=node1 \
+    cockroach start --insecure --store=node1 \
       --listen-addr=localhost:30001 \
-      --http-addr=localhost:40001
+      --http-addr=localhost:40001 \
       --join=xcnc30.comp.nus.edu.sg:30000,xcnc32.comp.nus.edu.sg:30002,xcnc33.comp.nus.edu.sg:30003,xcnc34.comp.nus.edu.sg:30004 \
       --background
 
   elif [ $h == 32 ]
   then
-    cockroach start --store=node2 \
+    cockroach start --insecure --store=node2 \
       --listen-addr=localhost:30002 \
-      --http-addr=localhost:40002
+      --http-addr=localhost:40002 \
       --join=xcnc30.comp.nus.edu.sg:30000,xcnc31.comp.nus.edu.sg:30001,xcnc33.comp.nus.edu.sg:30003,xcnc34.comp.nus.edu.sg:30004 \
       --background
 
   elif [ $h == 33 ]
   then
-    cockroach start --store=node3 \
+    cockroach start --insecure --store=node3 \
       --listen-addr=localhost:30003 \
-      --http-addr=localhost:40003
+      --http-addr=localhost:40003 \
       --join=xcnc30.comp.nus.edu.sg:30000,xcnc31.comp.nus.edu.sg:30001,xcnc32.comp.nus.edu.sg:30002,xcnc34.comp.nus.edu.sg:30004 \
       --background
 
   elif [ $h == 34 ]
   then
-    cockroach start --store=node4 \
+    cockroach start --insecure --store=node4 \
       --listen-addr=localhost:30004 \
-      --http-addr=localhost:40004
+      --http-addr=localhost:40004 \
       --join=xcnc30.comp.nus.edu.sg:30000,xcnc31.comp.nus.edu.sg:30001,xcnc32.comp.nus.edu.sg:30002,xcnc33.comp.nus.edu.sg:30003 \
       --background
 
   fi
-fi
 
-echo "Started server on : node${h}"
+  echo "\n\n*********\n\nStarted server on : node${h}"
+elif [ $1 == "stop" ]
+then
+  echo "Stopping the node : node${h}..."
+  if [ $h == 30 ]
+  then
+    cockroach quit --insecure --host=localhost:30000
+  elif [ $h == 31 ]
+  then
+    cockroach quit --insecure --host=localhost:30001
+  elif [ $h == 32 ]
+  then
+    cockroach quit --insecure --host=localhost:30002
+  elif [ $h == 33 ]
+  then
+    cockroach quit --insecure --host=localhost:30003
+  elif [ $h == 34 ]
+  then
+    cockroach quit --insecure --host=localhost:30004
+  fi
+
+fi
