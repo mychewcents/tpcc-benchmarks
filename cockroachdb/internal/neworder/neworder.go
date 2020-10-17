b1d7cdf5efa4d0f79bd07c78097a3577778807e8
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-10-17 22:21:10 +0530

    WIP - New Order Transaction

--- cockroachdb/internal/neworder/neworder.go
+++ cockroachdb/internal/neworder/neworder.go
@@ -25,13 +25,14 @@ type newOrder struct {
 
 type itemObject struct {
 	quantity, supplier int
+	remote             int
 }
 
 // ProcessTransaction process the new transaction
 func ProcessTransaction(db *sql.DB, warehouseID, districtID, customerID, numItems int, itemIDs, supplierWarehouseIDs, itemQtys []int) {
 
 	orderTable := fmt.Sprintf("ORDER_%d_%d", warehouseID, districtID)
-	orderLineTable := fmt.Sprintf("ORDER_LINE_%d_%d", warehouseID, districtID)
+	// orderLineTable := fmt.Sprintf("ORDER_LINE_%d_%d", warehouseID, districtID)
 
 	sqlStatement := fmt.Sprintf("UPDATE District SET D_NEXT_O_ID = D_NEXT_O_ID + 1 WHERE D_W_ID = %d AND D_ID = %d RETURNING D_NEXT_O_ID, D_TAX, D_W_TAX", warehouseID, districtID)
 
@@ -51,18 +52,26 @@ func ProcessTransaction(db *sql.DB, warehouseID, districtID, customerID, numItem
 	itemsToAttributesMap := make(map[int]itemObject)
 
 	for index, value := range itemIDs {
+		var remote int
+
+		if supplierWarehouseIDs[index] != warehouseID {
+			allLocalFlag = 0
+			remote = 1
+		}
+
 		itemsToAttributesMap[value] = itemObject{
 			supplier: supplierWarehouseIDs[index],
 			quantity: itemQtys[index],
+			remote:   remote,
 		}
+
 		if !itemVisited[value] {
 			itemVisited[value] = true
 			totalUniqueItems++
 		}
-		if allLocalFlag == 1 && supplierWarehouseIDs[index] != warehouseID {
-			allLocalFlag = 0
-		}
 	}
+
+	var totalAmount float64
 
 	err = crdb.ExecuteTx(context.Background(), db, nil, func(tx *sql.Tx) error {
 
@@ -72,15 +81,47 @@ func ProcessTransaction(db *sql.DB, warehouseID, districtID, customerID, numItem
 			return err
 		}
 
+		for key, value := range itemsToAttributesMap {
+			// Update Stock
+
+			var itemCurrQty int
+			row = tx.QueryRow("SELECT S_QUANTITY, S_YTD FROM STOCK WHERE S_W_ID = $1 AND S_I_ID = $2", value.supplier, key)
+			err = row.Scan(&itemCurrQty)
+
+			adjustedQty := itemCurrQty - value.quantity
+
+			if adjustedQty < 10 {
+				adjustedQty += 100
+			}
+
+			// S_W_ID, S_I_ID, S_QUANTITY, S_YTD, S_ORDER_CNT, S_REMOTE_CNT
+			row = tx.QueryRow("UPDATE STOCK SET S_QUANTITY = $1, S_YTD = S_YTD + $2, S_ORDER_CNT = S_ORDER_CNT + 1, S_REMOTE_CNT = $3 WHERE S_W_ID = $4 AND S_I_ID = $5 RETURNING S_I_NAME, S_I_PRICE",
+				adjustedQty,
+				value.quantity,
+				value.remote,
+				value.supplier,
+				key,
+			)
+
+			var itemName string
+			var itemPrice float64
+
+			err = row.Scan(&itemName, &itemPrice)
+
+			totalAmount += itemPrice * float64(value.quantity)
+
+			// Add a new Order Line Item String
+		}
+
 		return nil
 	})
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
 
-	printOutputState()
+	printOutputState(totalAmount)
 }
 
-func printOutputState() {
-	fmt.Println()
+func printOutputState(totalAmount float64) {
+	fmt.Println(totalAmount)
 }
