208376b1bebb173ce65de891fa187daee9cc40ba
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-10-17 18:59:49 +0530

    Adding the neworder transaction - WIP

--- cockroachdb/internal/neworder/neworder.go
+++ cockroachdb/internal/neworder/neworder.go
@@ -1,26 +1,86 @@
 package neworder
 
-type NewOrder struct {
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/cockroachdb/cockroach-go/crdb"
+)
+
+type newOrder struct {
 	// INPUTS
 	CustomerID, DistrictID, WarehouseID, NumItems int
 	ItemIDs, SupplierWarehouseIDs, ItemQuantities []int64
 
 	// OUTPUTS
-	lastName, creditStatus, orderTimestamp string
+	lastName, creditStatus, orderTimestamp               string
 	custDiscount, totalAmount, warehouseTax, districtTax float64
-	orderID int
-	itemNames []string
-	itemAmount []float64
-	itemStock []int
+	orderID                                              int
+	itemNames                                            []string
+	itemAmount                                           []float64
+	itemStock                                            []int
 }
 
-func (f *NewOrder) ProcessTransaction() string {
-	fmt.Printf("Hello Akarsh!")
-	fmt.Printf("%d", f.CustomerID)
-
-	return f.printOutputState()
+type itemObject struct {
+	quantity, supplier int
 }
 
-func (f *NewOrder) printOutputState() string {
-	return "constant for now"
+// ProcessTransaction process the new transaction
+func ProcessTransaction(db *sql.DB, warehouseID, districtID, customerID, numItems int, itemIDs, supplierWarehouseIDs, itemQtys []int) {
+
+	orderTable := fmt.Sprintf("ORDER_%d_%d", warehouseID, districtID)
+	orderLineTable := fmt.Sprintf("ORDER_LINE_%d_%d", warehouseID, districtID)
+
+	sqlStatement := fmt.Sprintf("UPDATE District SET D_NEXT_O_ID = D_NEXT_O_ID + 1 WHERE D_W_ID = %d AND D_ID = %d RETURNING D_NEXT_O_ID, D_TAX, D_W_TAX", warehouseID, districtID)
+
+	var newOrderID int
+	var districtTax, warehouseTax float64
+	row := db.QueryRow(sqlStatement)
+	err := row.Scan(&newOrderID, &districtTax, &warehouseTax)
+	if err != nil {
+		log.Fatalf("%v", err)
+		return
+	}
+
+	var totalUniqueItems int
+
+	allLocalFlag := 1
+	itemVisited := make(map[int]bool)
+	itemsToAttributesMap := make(map[int]itemObject)
+
+	for index, value := range itemIDs {
+		itemsToAttributesMap[value] = itemObject{
+			supplier: supplierWarehouseIDs[index],
+			quantity: itemQtys[index],
+		}
+		if !itemVisited[value] {
+			itemVisited[value] = true
+			totalUniqueItems++
+		}
+		if allLocalFlag == 1 && supplierWarehouseIDs[index] != warehouseID {
+			allLocalFlag = 0
+		}
+	}
+
+	err = crdb.ExecuteTx(context.Background(), db, nil, func(tx *sql.Tx) error {
+
+		sqlStatement = fmt.Sprintf("INSERT INTO %s (O_ID, O_D_ID, O_W_ID, O_C_ID, O_OL_CNT, O_ALL_LOCAL) VALUES ($1, $2, $3, $5, $6)", orderTable)
+
+		if _, err := tx.Exec(sqlStatement, newOrderID, districtID, warehouseID, customerID, totalUniqueItems, allLocalFlag); err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	printOutputState()
+}
+
+func printOutputState() {
+	fmt.Println()
 }
