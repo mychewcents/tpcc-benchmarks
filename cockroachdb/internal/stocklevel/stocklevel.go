3958511de8360595c513e9c874ff2bb7a056c46b
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-10-17 13:10:30 +0530

    stock level transaction

---
+++ cockroachdb/internal/stocklevel/stocklevel.go
@@ -0,0 +1,41 @@
+package stocklevel
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+// ProcessTransaction processes the Stock Level Transaction
+func ProcessTransaction(db *sql.DB, warehouseID, districtID, threshold, lastNOrders int) {
+	var totalItems int
+	var lastOrderID int
+
+	row := db.QueryRow(`SELECT o_next_id FROM district WHERE d_w_id=$1 AND d_id=$2`, warehouseID, districtID)
+
+	err := row.Scan(&lastOrderID)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	startOrderID := lastOrderID - lastNOrders
+
+	sqlStatement := fmt.Sprintf(`
+		SELECT COUNT(*) FROM Stock 
+		WHERE S_W_ID=%d 
+		AND S_QUANTITY < %d 
+		AND S_ID IN (
+			SELECT OL_I_ID FROM ORDER_LINE_%d_%d 
+			WHERE OL_O_ID < %d AND OL_O_ID >= %d
+		)`,
+		warehouseID, threshold, warehouseID, districtID, lastOrderID, startOrderID,
+	)
+
+	row = db.QueryRow(sqlStatement)
+	err = row.Scan(&totalItems)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	fmt.Println("Total Number of Items below threshold: ", totalItems, " , for Order IDs between ", lastOrderID-lastNOrders, " and ", lastOrderID)
+}
