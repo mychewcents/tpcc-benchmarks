11f7432bd2a03a50e6d52c3e822d469cd9fae1a3
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-10-18 23:42:19 +0530

    Adding the order line creation script

--- cockroachdb/cmd/init/main.go
+++ cockroachdb/cmd/init/main.go
@@ -20,8 +20,8 @@ func init() {
 }
 
 func main() {
-	createOrdersTables(2, 2)
-	createOrderLinesTables()
+	createOrdersTables(10, 10)
+	createOrderLinesTables(10, 10)
 }
 
 func createOrdersTables(warehouses, districts int) {
@@ -59,11 +59,48 @@ func createOrdersTables(warehouses, districts int) {
 				fmt.Println(err)
 			}
 
-			fmt.Printf("Compelete: %d %d", i, j)
+			fmt.Println("Compelete: ", i, j)
 		}
 	}
 }
 
-func createOrderLinesTables() {
+func createOrderLinesTables(warehouses, districts int) {
+	baseSQLStatement := `
+		DROP TABLE IF EXISTS defaultdb.ORDER_LINE_WID_DID;
+		CREATE TABLE IF NOT EXISTS defaultdb.ORDER_LINE_WID_DID (
+			OL_W_ID int,
+			OL_D_ID int,
+			OL_O_ID int,
+			OL_NUMBER int,
+			OL_I_ID int,
+			OL_DELIVERY_D timestamp,
+			OL_AMOUNT decimal(6,2),
+			OL_SUPPLY_W_ID int,
+			OL_QUANTITY decimal(2,0),
+			OL_DIST_INFO char(24),
+			INDEX (OL_O_ID),
+			INDEX (OL_I_ID),
+			PRIMARY KEY (OL_W_ID, OL_D_ID, OL_O_ID, OL_NUMBER),
+			CONSTRAINT FK_ORDER_LINE FOREIGN KEY (OL_W_ID, OL_D_ID, OL_O_ID) REFERENCES defaultdb.ORDERS_WID_DID (O_W_ID, O_D_ID, O_ID)
+		);
+		
+		INSERT INTO defaultdb.ORDER_LINE_WID_DID 
+		SELECT * FROM defaultdb.ORDER_LINE
+		WHERE O_W_ID = WID AND O_D_ID = DID;
+	`
 
+	for i := 1; i <= warehouses; i++ {
+		for j := 1; j <= districts; j++ {
+			finalSQLStatement := strings.ReplaceAll(baseSQLStatement, "WID", strconv.Itoa(i))
+			finalSQLStatement = strings.ReplaceAll(finalSQLStatement, "DID", strconv.Itoa(j))
+
+			// fmt.Println(finalSQLStatement)
+			_, err := db.Exec(finalSQLStatement)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println("Compelete: ", i, j)
+		}
+	}
 }
