6f0397c2d5c396bc0ad5dfd0fb6b54f00587f106
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-10-18 23:30:08 +0530

    Adding DB to the SQL Statement

--- cockroachdb/cmd/init/main.go
+++ cockroachdb/cmd/init/main.go
@@ -26,9 +26,9 @@ func main() {
 
 func createOrdersTables(warehouses, districts int) {
 	baseSQLStatement := `
-		DROP TABLE IF EXISTS ORDERS_WID_DID;
+		DROP TABLE IF EXISTS defaultdb.ORDERS_WID_DID;
 		
-		CREATE TABLE IF NOT EXISTS ORDERS_WID_DID (
+		CREATE TABLE IF NOT EXISTS defaultdb.ORDERS_WID_DID (
 			O_W_ID int,
 			O_D_ID int,
 			O_ID int,
@@ -40,17 +40,19 @@ func createOrdersTables(warehouses, districts int) {
 			O_TOTAL_AMOUNT decimal(12,2),
 			INDEX (O_C_ID),
 			PRIMARY KEY (O_W_ID, O_D_ID, O_ID),
-			CONSTRAINT FK_ORDERS FOREIGN KEY (O_W_ID, O_D_ID, O_C_ID) REFERENCES CUSTOMER (C_W_ID, C_D_ID, C_ID)
+			CONSTRAINT FK_ORDERS FOREIGN KEY (O_W_ID, O_D_ID, O_C_ID) REFERENCES defaultdb.CUSTOMER (C_W_ID, C_D_ID, C_ID)
 		);
 		
-		INSERT INTO ORDERS_WID_DID (O_W_ID, O_D_ID, O_ID, O_C_ID, O_CARRIER_ID, O_OL_CNT, O_ALL_LOCAL, O_ENTRY_D, O_TOTAL_AMOUNT) 
-		SELECT O_W_ID, O_D_ID, O_ID, O_C_ID, O_CARRIER_ID, O_OL_CNT, O_ALL_LOCAL, O_ENTRY_D, 0.0 FROM ORDERS 
+		INSERT INTO defaultdb.ORDERS_WID_DID (O_W_ID, O_D_ID, O_ID, O_C_ID, O_CARRIER_ID, O_OL_CNT, O_ALL_LOCAL, O_ENTRY_D, O_TOTAL_AMOUNT) 
+		SELECT O_W_ID, O_D_ID, O_ID, O_C_ID, O_CARRIER_ID, O_OL_CNT, O_ALL_LOCAL, O_ENTRY_D, 0.0 FROM defaultdb.ORDERS 
 		WHERE O_W_ID = WID AND O_D_ID = DID;
 	`
 
 	for i := 1; i <= warehouses; i++ {
 		for j := 1; j <= districts; j++ {
 			finalSQLStatement := strings.ReplaceAll(baseSQLStatement, "WID", strconv.Itoa(i))
+			fmt.Println(finalSQLStatement)
+
 			finalSQLStatement = strings.ReplaceAll(baseSQLStatement, "DID", strconv.Itoa(j))
 
 			fmt.Println(finalSQLStatement)
