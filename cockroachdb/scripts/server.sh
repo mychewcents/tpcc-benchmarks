83856fd1475859bd274a826873d4f4eddc033f62
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-10-19 13:28:28 +0530

    Correcting the script

--- cockroachdb/scripts/server.sh
+++ cockroachdb/scripts/server.sh
@@ -22,9 +22,9 @@ then
   if [ $h == 30 ]
   then
     cockroach start --insecure --store=/home/stuproj/cs4224m/crdb-node-files/node0 \
-      --listen-addr=0.0.0.0:30000 \
-      --http-addr=0.0.0.0:8080 \
-      --join=192.168.48.179:30000,192.168.48.180:30000,192.168.48.181:30000,192.168.48.182:30000,192.168.48.183:30000 \
+      --listen-addr=192.168.48.179:27000
+      --http-addr=0.0.0.0:40000 
+      --join=192.168.48.179:27000,192.168.48.180:27000,192.168.48.181:27000,192.168.48.182:27000,192.168.48.183:27000
       --background
 
     # cockroach init --insecure --host=0.0.0.0:30000
@@ -32,46 +32,45 @@ then
   elif [ $h == 31 ]
   then
     cockroach start --insecure --store=/home/stuproj/cs4224m/crdb-node-files/node1 \
-      --listen-addr=0.0.0.0:30000 \
-      --http-addr=0.0.0.0:8080 \
-      --join=192.168.48.179:30000,192.168.48.180:30000,192.168.48.181:30000,192.168.48.182:30000,192.168.48.183:30000 \
+      --listen-addr=192.168.48.180:27000
+      --http-addr=0.0.0.0:40000 
+      --join=192.168.48.179:27000,192.168.48.180:27000,192.168.48.181:27000,192.168.48.182:27000,192.168.48.183:27000
       --background
 
   elif [ $h == 32 ]
   then
     cockroach start --insecure --store=/home/stuproj/cs4224m/crdb-node-files/node2 \
-      --listen-addr=0.0.0.0:30000 \
-      --http-addr=0.0.0.0:8080 \
-      --join=192.168.48.179:30000,192.168.48.180:30000,192.168.48.181:30000,192.168.48.182:30000,192.168.48.183:30000 \
+      --listen-addr=192.168.48.181:27000
+      --http-addr=0.0.0.0:40000 
+      --join=192.168.48.179:27000,192.168.48.180:27000,192.168.48.181:27000,192.168.48.182:27000,192.168.48.183:27000
       --background
 
   elif [ $h == 33 ]
   then
     cockroach start --insecure --store=/home/stuproj/cs4224m/crdb-node-files/node3 \
-      --listen-addr=0.0.0.0:30000 \
-      --http-addr=0.0.0.0:8080 \
-      --join=192.168.48.179:30000,192.168.48.180:30000,192.168.48.181:30000,192.168.48.182:30000,192.168.48.183:30000 \
+      --listen-addr=192.168.48.182:27000
+      --http-addr=0.0.0.0:40000 
+      --join=192.168.48.179:27000,192.168.48.180:27000,192.168.48.181:27000,192.168.48.182:27000,192.168.48.183:27000
       --background
 
   elif [ $h == 34 ]
   then
     cockroach start --insecure --store=/home/stuproj/cs4224m/crdb-node-files/node4 \
-      --listen-addr=0.0.0.0:30000 \
-      --http-addr=0.0.0.0:8080 \
-      --join=192.168.48.179:30000,192.168.48.180:30000,192.168.48.181:30000,192.168.48.182:30000,192.168.48.183:30000 \
+      --listen-addr=192.168.48.183:27000
+      --http-addr=0.0.0.0:40000 
+      --join=192.168.48.179:27000,192.168.48.180:27000,192.168.48.181:27000,192.168.48.182:27000,192.168.48.183:27000
       --background
 
   fi
-
   printf "\n**********\nStarted server on : node${h}\n**********\n"
 elif [ $1 == "stop" ]
 then
   printf "**********\nStopping the node : node${h}\n**********\n\n"
-  cockroach quit --insecure --host=0.0.0.0:30000
+  cockroach quit --insecure --host=$(hostname):27000
   printf "\n**********\nStopped the node : node${h}\n**********\n"
 elif [ $1 == "init" ]
 then
   printf "**********\nInitializing the cluster : node${h}\n**********\n\n"
-  cockroach init --insecure --host=0.0.0.0:30000
+  cockroach init --insecure --host=$(hostname):27000
   printf "\n**********\nStopped the node : node${h}\n**********\n"
 fi
