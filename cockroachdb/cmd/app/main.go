583bed475c73fec33b792692931beed15b89f586
Aravind <aravindkm95@gmail.com>
2020-10-21 12:53:40 +0530

    Top balance, Order status, Payment transactions (#9)
    * payment transaction
    
    * fixed identation
    
    * load OL script
    
    * minor change
    
    * testing payment tx
    
    * change IP
    
    * change file path
    
    * order status txn
    
    * top balance txn, post partition sql scripts
    
    * minor change

--- cockroachdb/cmd/app/main.go
+++ cockroachdb/cmd/app/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
-
+	"fmt"
 	"github.com/mychewcents/ddbms-project/cockroachdb/internal/cdbconn"
-	"github.com/mychewcents/ddbms-project/cockroachdb/internal/neworder"
+	"github.com/mychewcents/ddbms-project/cockroachdb/internal/payment"
+	"github.com/mychewcents/ddbms-project/cockroachdb/internal/order_status"
+	"github.com/mychewcents/ddbms-project/cockroachdb/internal/top_balance"
 )
 
 var db *sql.DB
@@ -18,70 +20,42 @@ func init() {
 }
 
 func main() {
-	itemIDs := []int{
-		68195, 26567, 4114, 69343, 1836,
-		89294, 31975, 80487, 32423, 93383,
-		46279, 98511, 69351, 4679, 96983,
+
+	for true {
+
+	var transaction_type byte
+	_, err := fmt.Scanf("%c", &transaction_type)
+
+	if err != nil {
+		if err.Error() == "EOF" {
+			fmt.Println("Read EOF")
+		} else {
+			fmt.Println(err)
+		}
+		break
 	}
-	supplierIDs := []int{
-		1, 1, 1, 1, 1,
-		1, 1, 1, 1, 1,
-		1, 1, 1, 1, 1,
+
+	switch (transaction_type) {
+		case 'N':
+		case 'P':
+			var warehouseId, districtId, customerId int
+				var amount float64
+			fmt.Scanf(",%d,%d,%d,%f", &warehouseId, &districtId, &customerId, &amount)
+			payment.ProcessTransaction(db, warehouseId, districtId, customerId, amount)
+			break
+		case 'D':
+		case 'O':
+			var warehouseId, districtId, customerId int
+			fmt.Scanf(",%d,%d,%d", &warehouseId, &districtId, &customerId)
+			order_status.ProcessTransaction(db, warehouseId, districtId, customerId)
+			break
+		case 'S':
+		case 'I':
+		case 'T':
+			top_balance.ProcessTransaction(db)
+			break
+		case 'R':
+
+		}
 	}
-	quantitys := []int{
-		1, 5, 7, 3, 1,
-		3, 6, 10, 5, 9,
-		9, 8, 4, 6, 2,
-	}
-
-	neworder.ProcessTransaction(db, 1, 1, 1279, 15, itemIDs, supplierIDs, quantitys)
-
-	// for true {
-
-	// var transaction_type byte
-	// _, err := fmt.Scanf("%c", &transaction_type)
-
-	// if err != nil {
-	// 	if err.Error() == "EOF" {
-	// 		fmt.Println("Read EOF")
-	// 	} else {
-	// 		fmt.Println(err)
-	// 	}
-	// 	break
-	// }
-
-	// switch (transaction_type) {
-	// case 'N':
-	// 	var c_id, d_id, w_id, m int
-	// 	fmt.Scanf("%d %d %d %d", &c_id, &d_id, &w_id, &m)
-	// 	var ol_i_id = make([]int64, m)
-	// 	var ol_supply_w_id = make([]int64, m)
-	// 	var ol_quantity = make([]float32, m)
-	// 	for i := 0; i < m; i++ {
-	// 		fmt.Scanf("%d %d %f", &ol_i_id[i], &ol_supply_w_id[i], &ol_quantity[i])
-	// 	}
-	// 	var new_order_input model.NewOrderInput
-	// 	(&new_order_input).InitNewOrderInput(c_id, d_id, w_id, m, ol_i_id, ol_supply_w_id, ol_quantity)
-	// 	executors.NewOrderExecutor(new_order_input)
-	// 	new_order_input.PrintNewOrderInput()
-	// 	break
-	// 	case 'P':
-	// 		var c_w_id, c_d_id, c_id int64
-	// 		var payment float32
-	// 		fmt.Scanf("%d %d %d %f", &c_w_id, &c_d_id, &c_id, &payment)
-
-	// 		var payment_input model.PaymentInput
-	// 		(&payment_input).InitPaymentInput(c_w_id, c_d_id, c_id, payment)
-	// 		executors.PaymentExecutor(payment_input)
-	// 		payment_input.PrintPaymentInput()
-	// 		break
-	// 	case 'D':
-	// 	case 'O':
-	// 	case 'S':
-	// 	case 'I':
-	// 	case 'T':
-	// 	case 'R':
-
-	// 	}
-	// }
 }
