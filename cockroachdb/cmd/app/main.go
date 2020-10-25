fec2122043bc43a2aa5ba16a3820cc7974b1f61c
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-10-25 13:23:21 +0530

    Removing unrequired code

--- cockroachdb/cmd/app/main.go
+++ cockroachdb/cmd/app/main.go
@@ -39,41 +39,4 @@ func main() {
 
 		caller.ProcessRequest(db, scanner, txArgs)
 	}
-	// for true {
-
-	// 	var transaction_type byte
-	// 	_, err := fmt.Scanf("%c", &transaction_type)
-
-	// 	if err != nil {
-	// 		if err.Error() == "EOF" {
-	// 			fmt.Println("Read EOF")
-	// 		} else {
-	// 			fmt.Println(err)
-	// 		}
-	// 		break
-	// 	}
-
-	// 	switch transaction_type {
-	// 	case 'N':
-	// 	case 'P':
-	// 		var warehouseId, districtId, customerId int
-	// 		var amount float64
-	// 		fmt.Scanf(",%d,%d,%d,%f", &warehouseId, &districtId, &customerId, &amount)
-	// 		payment.ProcessTransaction(db, warehouseId, districtId, customerId, amount)
-	// 		break
-	// 	case 'D':
-	// 	case 'O':
-	// 		var warehouseId, districtId, customerId int
-	// 		fmt.Scanf(",%d,%d,%d", &warehouseId, &districtId, &customerId)
-	// 		order_status.ProcessTransaction(db, warehouseId, districtId, customerId)
-	// 		break
-	// 	case 'S':
-	// 	case 'I':
-	// 	case 'T':
-	// 		top_balance.ProcessTransaction(db)
-	// 		break
-	// 	case 'R':
-
-	// 	}
-	// }
 }
