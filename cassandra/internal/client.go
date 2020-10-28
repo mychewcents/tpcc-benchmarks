7d863fbfab771f18e528300b0659e9a058ac9e12
Shreya Shetty <shreya.s.shetty1812@gmail.com>
2020-10-29 00:26:03 +0800

    [Cassandra]-Add logs to file

--- cassandra/internal/client.go
+++ cassandra/internal/client.go
@@ -3,6 +3,7 @@ package cassandra_client
 import (
 	"bufio"
 	"encoding/xml"
+	"fmt"
 	"github.com/gocql/gocql"
 	"github.com/mychewcents/ddbms-project/cassandra/internal/common"
 	"github.com/mychewcents/ddbms-project/cassandra/internal/config"
@@ -10,12 +11,23 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
+var experimentId, clientId int
+
 func init() {
-	file, err := os.OpenFile("log/logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if len(os.Args) < 3 {
+		panic("need to supply experimentId and clientId")
+	}
+
+	experimentId, _ = strconv.Atoi(os.Args[1])
+	clientId, _ = strconv.Atoi(os.Args[2])
+
+	fileName := fmt.Sprintf("log/logs_exp_%v_client%v", experimentId, clientId)
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,7 +36,8 @@ func init() {
 }
 
 func Start() {
-	log.Println("Starting Client!")
+	log.Printf("Starting Client %v", clientId)
+	time.Sleep(10 * time.Second)
 
 	cassandraSession := makeCassandraSession()
 	reader := bufio.NewReader(os.Stdin)
@@ -37,6 +50,7 @@ func Start() {
 		r.HandleCommand(text)
 		text, _ = reader.ReadString('\n')
 	}
+	log.Printf("Stopping Client %v", clientId)
 }
 
 func makeCassandraSession() *common.CassandraSession {
@@ -44,7 +58,8 @@ func makeCassandraSession() *common.CassandraSession {
 
 	readCluster := gocql.NewCluster(cassandraConfig.Hosts...)
 	readCluster.Keyspace = cassandraConfig.Keyspace
-	readCluster.Timeout = time.Second * 10
+	readCluster.Timeout = time.Minute * 2
+	readCluster.NumConns = 10
 	if strings.ToUpper(cassandraConfig.ReadConsistency) == "ONE" {
 		readCluster.Consistency = gocql.One
 	} else {
@@ -54,7 +69,8 @@ func makeCassandraSession() *common.CassandraSession {
 
 	writeCluster := gocql.NewCluster(cassandraConfig.Hosts...)
 	writeCluster.Keyspace = cassandraConfig.Keyspace
-	writeCluster.Timeout = time.Second * 10
+	writeCluster.Timeout = time.Minute * 2
+	readCluster.NumConns = 10
 	if strings.ToUpper(cassandraConfig.WriteConsistency) == "ONE" {
 		writeCluster.Consistency = gocql.One
 	} else {
