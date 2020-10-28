c76b62a737844edd8badd7ee32a124cbe1b2b9ba
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-10-28 23:31:20 +0530

    Added the CSV to write the results too.

--- cockroachdb/cmd/app/main.go
+++ cockroachdb/cmd/app/main.go
@@ -4,11 +4,11 @@ import (
 	"bufio"
 	"database/sql"
 	"flag"
+	"fmt"
 	"os"
+	"sort"
 	"strings"
 	"time"
-	"sort"
-	"fmt"
 
 	caller "github.com/mychewcents/ddbms-project/cockroachdb/internal"
 	"github.com/mychewcents/ddbms-project/cockroachdb/internal/cdbconn"
@@ -46,37 +46,41 @@ func outputStats(latencies []float64) {
 	for _, latency := range latencies {
 		elapsedTime += latency
 	}
-	
+
 	throughput := float64(processedTxs) * 1000 / elapsedTime
 	avgLatency := elapsedTime / float64(processedTxs)
 	var medianLatency float64
-	if processedTxs % 2 > 0 {
-		medianLatency = latencies[(processedTxs - 1) / 2]
+	if processedTxs%2 > 0 {
+		medianLatency = latencies[(processedTxs-1)/2]
 	} else {
-		medianLatency = (latencies[processedTxs / 2] + latencies[processedTxs / 2 - 1]) / 2
+		medianLatency = (latencies[processedTxs/2] + latencies[processedTxs/2-1]) / 2
 	}
-	p99 := latencies[99 * processedTxs / 100]
-	p95 := latencies[95 * processedTxs / 100]
+	p99 := latencies[99*processedTxs/100]
+	p95 := latencies[95*processedTxs/100]
 
-	outputStr := "Total number of transactions processed: %d\n"
-	outputStr += "Total elapsed time: %fs\n"
-	outputStr += "Throughput: %f\n"
-	outputStr += "Average Latency: %fms\n"
-	outputStr += "Median Latency(ms): %fms\n"
-	outputStr += "p99 Latency(ms): %fms\n"
-	outputStr += "p95 Latency(ms): %fms"
+	// var outputStr strings.Builder
+	// outputStr.WriteString(fmt.Sprintf("Total number of transactions processed: %d\n", processedTxs))
+	// outputStr.WriteString(fmt.Sprintf("Total elapsed time: %fs\n", elapsedTime/1000))
+	// outputStr.WriteString(fmt.Sprintf("Throughput: %f\n", throughput))
+	// outputStr.WriteString(fmt.Sprintf("Average Latency: %fms\n", avgLatency))
+	// outputStr.WriteString(fmt.Sprintf("Median Latency(ms): %fms\n", medianLatency))
+	// outputStr.WriteString(fmt.Sprintf("p99 Latency(ms): %fms\n", p99))
+	// outputStr.WriteString(fmt.Sprintf("p95 Latency(ms): %fms", p95))
 
-	fmt.Println(
-		fmt.Sprintf(outputStr,
-			processedTxs,
-			elapsedTime / 1000,
-			throughput,
-			avgLatency,
-			medianLatency,
-			p99,
-			p95,
-		),
-	)
+	outputStr := fmt.Sprintf("%d,%d,%d,%f,%f,%f,%f,%f,%f", experiment, client, processedTxs, elapsedTime/1000, throughput, avgLatency, medianLatency, p99, p95)
+
+	csvFile, err := os.Create(fmt.Sprintf("%d-%d.csv", experiment, client))
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println(outputStr)
+		return
+	}
+	defer csvFile.Close()
+
+	if _, err := csvFile.WriteString(outputStr); err != nil {
+		fmt.Println(err)
+	}
+
 }
 
 func main() {
@@ -91,7 +95,7 @@ func main() {
 		start := time.Now()
 		status := caller.ProcessRequest(db, scanner, txArgs)
 		if status == true {
-			latencies = append(latencies, float64(time.Since(start)) / float64(time.Millisecond))
+			latencies = append(latencies, float64(time.Since(start))/float64(time.Millisecond))
 		}
 	}
 	outputStats(latencies)
