d891234dea71a13960f2eb97b3cb77f511604801
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-11-01 13:28:16 +0530

    Creating dev and prod execution scripts

--- cockroachdb/scripts/run.sh
+++ cockroachdb/scripts/run.sh
@@ -1,51 +1,40 @@
 #! /bin/sh
 
-exp=$1
-server=$2
-skip=5
+env=$1
+exp=$2
+host_or_client=$3
 
-if [ "$#" -eq 2 ] 
+if [ "$#" -eq 3 ] 
 then
   if [ $exp -gt 4 ] && [ $exp -lt 9 ] 
   then
-
-    host=192.168.49.179
-    case $server in
-      2) host=192.168.49.180
-      ;;
-      3) host=192.168.49.181
-      ;;
-      4) host=192.168.49.182
-      ;;
-      5) host=192.168.49.183
-      ;;
-    esac
-
-    go build -o app ../cmd/app/main.go
+    go build -o app cmd/app/main.go
     chmod a+x ./app
-    
-    case $exp in
-      5) 
-      clients=5
-      skip=4
-      ;;
-      6) clients=4
-      skip=5
-      ;;
-      7) clients=10
-      skip=4
-      ;;
-      8) clients=8
-      skip=5
-      ;;
-    esac
 
-    tx=$server
+    if [ $env == 'dev' ]
+    then
+      ./app -exp=$exp -client=$host_or_client < assets/data/transactions/$host_or_client.txt
+    else 
+      skip=5
+      total=20
 
-    for (( i=tx; i<=clients*skip; i=i+skip ))
-    do
-      # echo $host $i
-      ./app -exp=$exp -client=$i -host=$host -port=26257 < $i.txt &
-    done
+      case $exp in
+        5) skip=4
+        ;;
+        6) skip=5
+        ;;
+        7) skip=4
+        total=40
+        ;;
+        8) skip=5
+        total=40
+        ;;
+      esac
+
+      for (( i=host_or_client; i<=total; i=i+skip ))
+      do
+        ./app -exp=$exp -client=$i -config=configs/prod/node_$host_or_client.json < assets/data/transactions/$i.txt &
+      done
+    fi
   fi
 fi
