990b4bf3607728001cd3a9278d83e50f993cec22
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-11-01 14:56:30 +0530

    Adding the command to upload the data files

--- cockroachdb/scripts/server.sh
+++ cockroachdb/scripts/server.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+node_store=/temp/cs5424-team-m/cdb-server/node-files
+
 if [ $1 == "help" ]
 then
   echo
@@ -11,10 +13,11 @@ then
   echo
   echo
   echo "First Argument - action to perform"
-  echo "   - init  : To initialize the Cockroach DB Instance"
-  echo "   - start : To start the CockroachDB server on the localhost"
-  echo "   - stop  : To stop the local running instance of the Cockroach DB"
-  echo "   - sql   : To start the SQL Client for the localhost"
+  echo "   - init     : To initialize the Cockroach DB Instance"
+  echo "   - start    : To start the CockroachDB server on the localhost"
+  echo "   - stop     : To stop the local running instance of the Cockroach DB"
+  echo "   - sql      : To start the SQL Client for the localhost"
+  echo "   - upload   : To upload the init files to node's directory"
   echo
   echo
   echo "Second Argument - host number to be used - Only used when creating directories"
@@ -31,7 +34,7 @@ then
     if [ $2 -gt 0 ] && [ $2 -lt 6 ]
     then
       printf "**********\nStarting Cockroach DB node on : node${2}\n**********\n\n"
-      echo cockroach start --insecure --store=/home/stuproj/cs4224m/crdb-node-files/node$2 \
+      echo cockroach start --insecure --store=$node_store/node$2 \
         --listen-addr=$(hostname -i):27000 \
         --http-addr=0.0.0.0:40000 \
         --join=192.168.48.179:27000,192.168.48.180:27000,192.168.48.181:27000,192.168.48.182:27000,192.168.48.183:27000 \
@@ -56,6 +59,16 @@ then
     printf "**********\nStarting the SQL Client : node${2}\n***********\n\n"
     cockroach sql --insecure --host=$(hostname -i):27000
     printf "\n**********\nStopped the SQL Client : node${2}\n**********\n\n"
+  elif [ $1 == "upload" ]
+  then
+    printf "**********\nUploadng the init files : node${2}\n***********\n\n"
+    orig=$GOPATH/src/github.com/mychewcents/ddbms-project/cockroachdb/assets/data/raw
+    cd $orig
+    for FILE in *; 
+    do 
+      cp $FILE $node_store/node$2/extern/assets/raw/$FILE --insecure --host=$(hostname -i):27000
+    done
+    printf "\n**********\nUploaded the init files : node${2}\n**********\n\n"
   fi
 else
   echo "Use the command \"cdbserv help\" to learn more about the acceptable parameters"
