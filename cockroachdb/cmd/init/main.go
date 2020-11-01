f9a3b479900e5658d4b2a1999dfac9ccdb096f0f
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-11-02 00:46:12 +0530

    Syntax error in OL

--- cockroachdb/cmd/init/main.go
+++ cockroachdb/cmd/init/main.go
@@ -164,7 +164,7 @@ func createOrderLinesTables(warehouses, districts int) error {
 			OL_DIST_INFO char(24),
 			INDEX (OL_O_ID),
 			INDEX (OL_I_ID),
-			PRIMARY KEY (OL_W_ID, OL_D_ID, OL_O_ID, OL_NUMBER)
+			PRIMARY KEY (OL_W_ID, OL_D_ID, OL_O_ID, OL_NUMBER),
 			CONSTRAINT FK_ORDER_LINE FOREIGN KEY (OL_W_ID, OL_D_ID, OL_O_ID) REFERENCES defaultdb.ORDERS_WID_DID (O_W_ID, O_D_ID, O_ID)
 		);
 		
