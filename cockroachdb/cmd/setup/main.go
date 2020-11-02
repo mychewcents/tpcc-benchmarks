326b336af4b97099dff56d4b6e67821eb6fc436c
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-11-02 18:02:07 +0530

    Adding dynamic setup instructions

---
+++ cockroachdb/cmd/setup/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+)
+
+type configuration struct {
+	DownloadURL string `json:"data_files_url"`
+	WorkingDir  string `jdon:"working_dir"`
+	Nodes       []node `json:"nodes"`
+}
+
+type node struct {
+	Host string `json:"host"`
+	Port int    `json:"port"`
+}
+
+func main() {
+	var setupConfigFile string
+
+	if len(os.Args) != 3 {
+		fmt.Println("No custom configuration file. Using the \"dev\" configuration file: configs/dev/setup.json file.")
+		setupConfigFile = "configs/dev/setup.json"
+	} else if os.Args[1] == "dev" || os.Args[1] == "prod" {
+		setupConfigFile = os.Args[2]
+	} else {
+		panic("use \"dev\" or \"prod\" as the first argument and pass the \"configuration file\" in the second arugment")
+	}
+
+	configFile, err := os.Open(setupConfigFile)
+	if err != nil {
+		panic("file cannot be read")
+	}
+
+	byteValue, _ := ioutil.ReadAll(configFile)
+
+	var config configuration
+
+	if err = json.Unmarshal(byteValue, &config); err != nil {
+		panic(err)
+	}
+
+	configFile.Close()
+
+	cmd := exec.Command("./scripts/init_setup.sh", os.Args[1], config.WorkingDir, config.DownloadURL)
+
+	err = cmd.Start()
+	if err != nil {
+		log.Fatalf("Err: %v", err)
+	}
+	log.Printf("Waiting for command to finish...")
+	err = cmd.Wait()
+	log.Printf("Command finished with error: %v", err)
+}
--- cockroachdb/scripts/init_setup.sh
+++ cockroachdb/scripts/init_setup.sh
@@ -6,6 +6,7 @@
 # and transaction.
 ############################################################
 
+
 if [ $1 == 'help' ]
 then
   echo
@@ -22,41 +23,54 @@ then
   echo "   - local  : Only downloads the project files"
   echo
   echo
+  echo "Second Argument - Location of the working directory"
+  echo "   - <path>   : Path to the working directory for the nodes and executables"
+  echo
+  echo
+  echo "Third Argument - URL for the project file downloads"
+  echo "   - <URL>    : To download the files for the project"
+  echo
+  echo
   echo "Happy Running!"
   echo
   echo
-elif [ $1 == 'prod' ] || [ $1 == 'local' ]
+elif [ "$#" -eq 3 ]
 then
-  if [ $1 == 'prod' ]
+  if [ $1 == 'prod' ] || [ $1 == 'local' ]
   then
-    if [ ! -d "/temp/cs5424-team-m/cdb-server" ]
+    if [ $1 == 'prod' ]
     then
-      echo "Creating the server installation script directory"
-      mkdir /temp/cs5424-team-m/cdb-server
+      if [ ! -d "${2}/cdb-server" ]
+      then
+        echo "Creating the server installation script directory"
+        mkdir -p ${2}/cdb-server
+      fi
+
+      cp scripts/server.sh cdbserv
+      chmod a+x cdbserv
+      mv cdbserv ${2}/cdb-server/
+
+      # cp scripts/run.sh cdbclient
+      # chmod a+x cdbclient
+      # mv cdbclient /temp/cs5424-team-m/cdb-server/
     fi
 
-    cp scripts/server.sh cdbserv
-    chmod a+x cdbserv
-    mv cdbserv /temp/cs5424-team-m/cdb-server/
+    rm -rf assets/data
 
-    # cp scripts/run.sh cdbclient
-    # chmod a+x cdbclient
-    # mv cdbclient /temp/cs5424-team-m/cdb-server/
+    mkdir assets/data
+    mkdir assets/data/raw
+    mkdir assets/data/transactions
+
+    # curl $3 -L -o assets/project-files.zip
+    # unzip assets/project-files.zip -d assets
+    # mv assets/project-files/data-files/* assets/data/raw
+    # mv assets/project-files/xact-files/* assets/data/transactions
+
+    # rm assets/project-files.zip
+    # rm -rf assets/project-files
+  else
+    echo "Use the \"help\" command to learn more about the arguments"
   fi
-
-  rm -rf assets/data
-
-  mkdir assets/data
-  mkdir assets/data/raw
-  mkdir assets/data/transactions
-
-  curl 'http://www.comp.nus.edu.sg/~cs4224/project-files.zip' -L -o assets/project-files.zip
-  unzip assets/project-files.zip -d assets
-  mv assets/project-files/data-files/* assets/data/raw
-  mv assets/project-files/xact-files/* assets/data/transactions
-
-  rm assets/project-files.zip
-  rm -rf assets/project-files
 else
-  echo "Use the \"help\" command to learn more about the arguments"
+    echo "Use the \"help\" command to learn more about the arguments"
 fi
