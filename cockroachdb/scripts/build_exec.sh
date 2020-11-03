5534d6ad3d76bae1d32fa41d8970130be8123343
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-11-03 14:13:51 +0530

    Adding the correct binary names

--- cockroachdb/scripts/build_exec.sh
+++ cockroachdb/scripts/build_exec.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 
-go build -o setup cmd/setup/main.go
+go build -o setupCmd cmd/setup/main.go
 echo "./setup -env=prod -node=1 -config=configs/prod/setup_5.json"
 
-go build -o server cmd/server/main.go
+go build -o serverCmd cmd/server/main.go
 echo "./server -env=prod -node=1 -config=configs/prod/setup_5.json (start | stop | init | sql | load)"
 
-go build -o init cmd/init/main.go
-go build -o client cmd/app/main.go
-go build -o dbstate cmd/dbstate/main.go
+go build -o clientCmd cmd/app/main.go
+go build -o dbstateCmd cmd/dbstate/main.go
