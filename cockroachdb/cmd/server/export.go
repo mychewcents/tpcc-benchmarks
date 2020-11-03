c5d01e3e1d768a2f666f4c90eb3ae0c12ca17773
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-11-04 05:07:10 +0530

    Adding the export scripts to export the data

--- cockroachdb/cmd/server/export.go
+++ cockroachdb/cmd/server/export.go
@@ -9,30 +9,53 @@ import (
 	"github.com/mychewcents/ddbms-project/cockroachdb/internal/connection/config"
 )
 
+type sqlStatement struct {
+	baseStatement string
+	filePath      string
+}
+
 func export(c config.Configuration) {
 	hostName := fmt.Sprintf("%s:%d", c.HostNode.Host, c.HostNode.Port)
-	baseSQLStatement := fmt.Sprintf("SELECT * FROM ORDER_ITEMS_CUSTOMERS_WID_DID;")
-	for w := 1; w <= 10; w++ {
-		for d := 1; d <= 10; d++ {
-			finalSQLStatement := strings.ReplaceAll(baseSQLStatement, "WID", fmt.Sprintf("%d", w))
-			finalSQLStatement = strings.ReplaceAll(finalSQLStatement, "DID", fmt.Sprintf("%d", d))
-			fileName := fmt.Sprintf("assets/processed/item_pairs/%d_%d.csv", w, d)
-
-			cmd := &exec.Cmd{
-				Path: "scripts/export_data.sh",
-				Args: []string{"scripts/export_data.sh",
-					hostName,
-					finalSQLStatement,
-					fileName,
-				},
-				Stdout: os.Stdout,
-				Stderr: os.Stderr,
-				Dir:    ".",
-			}
-
-			cmd.Start()
-			cmd.Wait()
-		}
-		fmt.Printf("Completed: w = %d\n", w)
+	sqls := []sqlStatement{
+		{
+			baseStatement: "SELECT * FROM ORDERS_WID_DID",
+			filePath:      "orders",
+		},
+		{
+			baseStatement: "SELECT * FROM ORDER_LINE_WID_DID",
+			filePath:      "order_line",
+		},
+		{
+			baseStatement: "SELECT * FROM ORDER_ITEMS_CUSTOMERS_WID_DID",
+			filePath:      "item_pairs",
+		},
 	}
+
+	for _, value := range sqls {
+		baseSQLStatement := value.baseStatement
+		for w := 1; w <= 10; w++ {
+			for d := 1; d <= 10; d++ {
+				finalSQLStatement := strings.ReplaceAll(baseSQLStatement, "WID", fmt.Sprintf("%d", w))
+				finalSQLStatement = strings.ReplaceAll(finalSQLStatement, "DID", fmt.Sprintf("%d", d))
+				fileName := fmt.Sprintf("assets/processed/%s/%d_%d.csv", value.filePath, w, d)
+
+				cmd := &exec.Cmd{
+					Path: "scripts/export_data.sh",
+					Args: []string{"scripts/export_data.sh",
+						hostName,
+						finalSQLStatement,
+						fileName,
+					},
+					Stdout: os.Stdout,
+					Stderr: os.Stderr,
+					Dir:    ".",
+				}
+
+				cmd.Start()
+				cmd.Wait()
+			}
+			fmt.Printf("Completed: w = %d\n", w)
+		}
+	}
+
 }
