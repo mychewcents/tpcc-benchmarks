00c86f284bf4ff3640ab187b857eb284ac7278cb
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-11-04 17:56:32 +0530

    Removing repeated logs

--- cockroachdb/internal/init/processed/create.go
+++ cockroachdb/internal/init/processed/create.go
@@ -11,7 +11,6 @@ import (
 func CreateParent(c config.Configuration) error {
 	log.Println("Creating tables...")
 
-	log.Println("\nExecuting the SQL: scripts/sql/processed/create.sql")
 	if err := tables.ExecuteSQL(c, "scripts/sql/processed/create.sql"); err != nil {
 		log.Fatalf("error occured while creating processed tables. Err: %v", err)
 		return err
@@ -25,7 +24,6 @@ func CreateParent(c config.Configuration) error {
 func CreatePartitions(c config.Configuration) error {
 	log.Println("Creating partitions of tables...")
 
-	log.Println("Executing the SQL: scripts/sql/processed/create-partitions.sql")
 	if err := tables.ExecuteSQLForPartitions(c, 10, 10, "scripts/sql/processed/create-partitions.sql"); err != nil {
 		log.Fatalf("error occured while creating partitions. Err: %v", err)
 		return err
