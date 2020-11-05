7f6078ef72b80ab2863c575a5ad68e08781daab6
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-11-05 11:30:09 +0530

    Removing the update issue with the orders

--- cockroachdb/scripts/sql/processed/load.sql
+++ cockroachdb/scripts/sql/processed/load.sql
@@ -3,5 +3,3 @@ IMPORT INTO DISTRICT CSV DATA ('nodelocal://1/assets/processed/district/district
 IMPORT INTO CUSTOMER CSV DATA ('nodelocal://1/assets/processed/customer/customer.csv') WITH skip='1', nullif='NULL';
 IMPORT INTO ITEM CSV DATA ('nodelocal://1/assets/processed/item/item.csv') WITH skip='1', nullif='NULL';
 IMPORT INTO STOCK CSV DATA ('nodelocal://1/assets/processed/stock/stock.csv') WITH skip='1', nullif='NULL';
-IMPORT INTO ORDERS CSV DATA ('nodelocal://1/assets/processed/order/order.csv') WITH skip='1', nullif='NULL';
-IMPORT INTO ORDER_LINE CSV DATA ('nodelocal://1/assets/processed/orderline/orderline.csv') WITH skip='1', nullif='NULL';
