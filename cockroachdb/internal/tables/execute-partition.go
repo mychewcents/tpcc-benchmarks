681b4db0d2df181cf0147f1d760e9d9690714e9f
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-11-06 07:36:56 +0530

    Reducing the number of threads

--- cockroachdb/internal/tables/execute-partition.go
+++ cockroachdb/internal/tables/execute-partition.go
@@ -1,6 +1,7 @@
 package tables
 
 import (
+	"database/sql"
 	"errors"
 	"io/ioutil"
 	"log"
@@ -26,18 +27,18 @@ func ExecuteSQLForPartitions(c config.Configuration, warehouses, districts int, 
 	byteValue, _ := ioutil.ReadAll(sqlFile)
 	baseSQLStatement := string(byteValue)
 
-	ch := make(chan bool, 100)
+	ch := make(chan bool, 10)
 	errFound := false
-	for w := 1; w <= warehouses; w++ {
-		for d := 1; d <= districts; d++ {
-			finalSQLStatement := strings.ReplaceAll(baseSQLStatement, "WID", strconv.Itoa(w))
-			finalSQLStatement = strings.ReplaceAll(finalSQLStatement, "DID", strconv.Itoa(d))
-
-			go executeParallel(c, w, d, finalSQLStatement, ch)
-		}
+	db, err := cdbconn.CreateConnection(c.HostNode)
+	if err != nil {
+		panic("load function couldn't create a connection to the server")
 	}
 
-	for i := 0; i < 100; i++ {
+	for w := 1; w <= 10; w++ {
+		go executeParallel(db, w, baseSQLStatement, ch)
+	}
+
+	for i := 0; i < 10; i++ {
 		<-ch
 	}
 
@@ -49,17 +50,18 @@ func ExecuteSQLForPartitions(c config.Configuration, warehouses, districts int, 
 	return nil
 }
 
-func executeParallel(c config.Configuration, w, d int, finalSQLStatement string, ch chan bool) {
-	db, err := cdbconn.CreateConnection(c.HostNode)
-	if err != nil {
-		panic("load function couldn't create a connection to the server")
+func executeParallel(db *sql.DB, w int, baseSQLStatement string, ch chan bool) {
+	for d := 1; d <= 10; d++ {
+		finalSQLStatement := strings.ReplaceAll(baseSQLStatement, "WID", strconv.Itoa(w))
+		finalSQLStatement = strings.ReplaceAll(finalSQLStatement, "DID", strconv.Itoa(d))
+
+		if _, err := db.Exec(finalSQLStatement); err != nil {
+			log.Println(finalSQLStatement)
+			log.Fatalf("Err: %v", err)
+			ch <- false
+		}
 	}
 
-	if _, err := db.Exec(finalSQLStatement); err != nil {
-		log.Println(finalSQLStatement)
-		log.Fatalf("Err: %v", err)
-		ch <- false
-	}
-	log.Printf("Executed partition: %d %d", w, d)
+	log.Printf("Executed partition: w=%d", w)
 	ch <- true
 }
