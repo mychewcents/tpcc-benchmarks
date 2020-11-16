7d64f56e151cfe202b68d13492f20975a2809764
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-11-16 19:16:10 +0530

    Adding the router

--- cockroachdb/internal/router/router.go
+++ cockroachdb/internal/router/router.go
@@ -21,13 +21,14 @@ func GetNewRouter(db *sql.DB) *TransactionRouter {
 	handlers["P"] = controller.CreatePaymentController(db)
 	handlers["I"] = controller.CreatePopularItemController(db)
 	handlers["T"] = controller.CreateTopBalanceController(db)
+	handlers["D"] = controller.CreateDeliveryController(db)
 
 	return &TransactionRouter{handlers: handlers}
 }
 
 // ProcessTransaction processes each transaction upon input
 func (tr *TransactionRouter) ProcessTransaction(scanner *bufio.Scanner, args []string) (completed bool) {
-	if args[0] == "N" || args[0] == "S" || args[0] == "P" || args[0] == "I" || args[0] == "T" {
+	if args[0] == "N" || args[0] == "S" || args[0] == "P" || args[0] == "I" || args[0] == "T" || args[0] == "D" {
 		completed = tr.handlers[args[0]].HandleTransaction(scanner, args[1:])
 	}
 
